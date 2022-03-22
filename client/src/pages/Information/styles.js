03345156f5619864cf62e1aab30065ba7a3a54f9
ZacWarner <zektgn@gmail.com>
2022-03-22 12:00:55 -0700

    fixed styling for mobile

--- client/src/pages/Information/styles.js
+++ client/src/pages/Information/styles.js
@@ -2,13 +2,19 @@ export default (theme) => ({
     root: {
         height: '100vh',
         paddingTop: '5rem',
+        paddingBottom: '5rem',
         backgroundColor: "#800000",
-        color: 'white'
+        color: 'white',
+        [theme.breakpoints.down('sm')]: {
+            height: '100%',
+        }
     },
     image: {
         width: '100%',
-        margin: theme.spacing(2)
-    
+        margin: theme.spacing(2),  
+        [theme.breakpoints.down('md')]: {
+            margin: 0,
+        }
     },
     divider: {
         margin: theme.spacing(2),
